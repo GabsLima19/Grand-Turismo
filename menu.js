865e17466d74c758ccf62f21ac2f3e00eb31ecf5
Gabriel Lima <49871125+GabsLima19@users.noreply.github.com>
2023-01-28 01:29:25 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,79 @@
+document.write(`
+
+<header>
+<nav id='cssmenu'>
+  <div class="logo"><a href="/home.html"><img src="/imagens/Logobeta2.png" width="40px" height="23.5px">Grand Turismo</a></div>
+  <div id="head-mobile"></div>
+  <div class="button"></div>
+  <ul>
+    <li class='active'><a href='/home.html'>HOME</a></li>
+    <li><a href='/sobre/sobre.html'>SOBRE</a></li>
+
+    <li><a href='#'>LOCAIS</a>
+      <ul>
+        <li><a href='#'>Arquitetura e Paisagens</a>
+          <ul>
+            <li><a href='/dados/arquipaisagem/catedral.html'>Catedral</a></li>
+            <li><a href='/dados/arquipaisagem/cidade_das_artes.html'>Cidade das Artes</a></li>
+            <li><a href='/dados/arquipaisagem/cristo_redentor.html'>Cristo Redentor</a></li>
+            <li><a href='/dados/arquipaisagem/pao_de_acucar.html'>Pão de Açucar</a></li>
+            <li><a href='/dados/arquipaisagem/pavilhao.html'>Feira de São Cristovão</a></li>
+          </ul>
+        </li>
+
+        <li><a href='#'>Museus</a>
+          <ul>
+            <li><a href='/dados/museus/museu_de_arte_moderna.html'>Museu de Arte Moderna</a></li>
+            <li><a href='/dados/museus/museu_de_arte.html'>Museu de Arte do Rio</a></li>
+            <li><a href='/dados/museus/museu_do_amanha.html'>Museu do Amanhã</a></li>
+            <li><a href='/dados/museus/museu_nacional.html'>Museu de Belas Artes</a></li>
+            <li><a href='/dados/museus/palacio_do_catete.html'>Palácio do Catete</a></li>
+          </ul>
+        </li>
+
+        <li><a href='#'>Praças</a>
+          <ul>
+            <li><a href='/dados/praças/cinelandia.html'>Cinelândia</a></li>
+            <li><a href='/dados/praças/general_tiburcio.html'>General Tiburcio</a></li>
+            <li><a href='/dados/praças/largo_carioca.html'>Largo da Carioca</a></li>
+            <li><a href='/dados/praças/praca_da_cruz_vermelha.html'>Praça da Cruz Vermelha</a></li>
+            <li><a href='/dados/praças/praca_paris.html'>Praça Paris</a></li>
+            <li><a href='/dados/praças/praca_xv.html'>Praça XV</a></li>
+          </ul>
+        </li>
+
+        <li><a href='#'>Praias</a>
+          <ul>
+            <li><a href='/dados/praias/copacabana.html'>Copacabana</a></li>
+            <li><a href='/dados/praias/grumari.html'>Grumari</a></li>
+            <li><a href='/dados/praias/ipanema.html'>Ipanema</a></li>
+            <li><a href='/dados/praias/leblon.html'>Leblon</a></li>
+            <li><a href='/dados/praias/prainha.html'>Prainha</a></li>
+          </ul>
+        </li>
+
+        <li><a href='#'>Restaurantes</a>
+          <ul>
+            <li><a href='/dados/restaurantes/cipriani.html'>Cipriani</a></li>
+            <li><a href='/dados/restaurantes/esplanada_grill.html'>Esplanada Grill</a></li>
+            <li><a href='/dados/restaurantes/fasano_al_mare.html'>Fasano al Mare</a></li>
+            <li><a href='/dados/restaurantes/laguiole.html'>Laguiole</a></li>
+            <li><a href='/dados/restaurantes/lasai.html'>Lasai</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+
+    <li><a href='#'>MULTIMIDIAS</a>
+      <ul>
+          <li><a href='/multimidia_dados/fotos.html'>Fotos</a></li>
+      </ul>
+    </li>
+
+    <li><a href='/contato/contato.html'>CONTATO</a></li>
+  </ul>
+
+</nav>
+</header>
+
+`);
